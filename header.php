3e064a0db078ba16f54dd016d1102d5c51e768bd
Steve Ryan <steve.ryan@asu.edu>
2017-05-26 15:05:37 -0700

    Fix CSS rollover in footer. Address console error
    Console error had to do with GA tag and datalayer declaration.
    CSS adjustment for the Endorsed Logo widget - grows to 105% not 110% now.

--- header.php
+++ header.php
@@ -35,14 +35,14 @@
 </head>
 <body <?php body_class(); ?>>  
 
-	<!-- Google Tag Manager ASU Universal-->
+	<!-- Google Tag Manager ASU Universal -->
 	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDWN8Z"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','SI_dataLayer','GTM-KDWN8Z');</script>
+	})(window,document,'script','dataLayer','GTM-KDWN8Z');</script>
 	<!-- End Google Tag Manager ASU Universal -->
 
 	<div id="asu-header">
